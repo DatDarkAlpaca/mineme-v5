53b78aab88c02d5281640654feb16c83f2019eef
Alpaca <paulocastrogalvao@hotmail.com>
2025-02-17 19:56:13 +0900

    Adds requirements and venv

--- run_client.bat
+++ run_client.bat
@@ -1,3 +1,6 @@
 @echo off
 set PYTHONPATH=.\;%PYTHONPATH%
+
+call .\venv\Scripts\activate.bat
 py -B mineme_client/src/main.py
+call .\venv\Scripts\deactivate.bat
--- run_server.bat
+++ run_server.bat
@@ -1,3 +1,6 @@
 @echo off
 set PYTHONPATH=.\;%PYTHONPATH%
+
+call .\venv\Scripts\activate.bat
 py -B mineme_server/src/main.py
+call .\venv\Scripts\deactivate.bat
